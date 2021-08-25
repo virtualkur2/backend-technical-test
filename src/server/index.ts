7c68f9aea62f8e732feb0839a48338d7634c23dd
Mauricio Contreras <virtual.kur2@gmail.com>
2021-08-25 08:10:42 +0200

    Express Aplication Class created

---
+++ src/server/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+
+/**
+ * Express Application Class
+ *
+ * @constructor
+ * @param {number} port - TCP port for server to listen up for requests.
+ * @param {string} baseUrl - Base URL for app listening requests.
+ *
+ */
+class Server {
+  public app: express.Application;
+  public port: number;
+  public baseURL: string;
+
+  constructor(port: number, baseURL?: string) {
+    this.port = port;
+    this.baseURL = baseURL || "/";
+    this.app = express();
+  }
+  /**
+   * Executes listen() method on Express Application
+   */
+  public listen() {
+    this.app.listen(this.port, () => {
+      console.log(
+        `App started and listening requests on TCP port: ${this.port}, at: "${this.baseURL}"`
+      );
+    });
+  }
+}
+
+export { Server };
